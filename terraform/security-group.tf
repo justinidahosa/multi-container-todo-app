8f31a26f17d7b926907fa7645d729d2e834b0f71
justin <justinidahosa@gmail.com>
2025-10-01 16:26:47 -0500

    added github actions workflow

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "todo_sg" {
    vpc_id = aws_vpc.my_vpc.id
-   
+
   name        = "todo-sg"
-  description = "allows ssh from my ip and listens on port 3000 for http from anywhere"
+  description = "allows ssh from everywhere so github actions can access it and listens on port 3000 for http from anywhere"
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["104.3.254.193/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 3000
